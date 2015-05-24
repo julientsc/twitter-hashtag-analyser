3feb5bdd45eec350e42b62e1ae7549544e6d1743
Julien Tscherrig <julien.tscherrig@hefr.ch>
2015-05-24 03:13:50 +0200

    Create the tweet collection based on the Recordable class

---
+++ src/main/java/model/TweetCollection.java
@@ -0,0 +1,73 @@
+package model;
+
+import model.tweet.MyTweet;
+import twitter4j.Status;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class TweetCollection {
+
+    public static final String PATH_TWEETS = "tweets.json";
+    public static final String PATH_HASHTAGS = "hashtags.json";
+
+    private static TweetCollection INSTANCE = null;
+
+    public static final long SAVE_COUNT = 500;
+    private long count = 0;
+
+    private Recordable<HashMap<Long, MyTweet>> myTweets = null;
+    private Recordable<HashMap<String, List<Long>>> myHashtags = null;
+
+    private TweetCollection() throws Exception {
+        this.myTweets = new Recordable<HashMap<Long, MyTweet>>(PATH_TWEETS, new HashMap<Long, MyTweet>().getClass());
+        this.myHashtags = new Recordable<HashMap<String, List<Long>>>(PATH_HASHTAGS, new HashMap<String, List<Long>>().getClass());
+    }
+
+    public static TweetCollection getInstance() throws Exception {
+        if(INSTANCE == null) {
+            INSTANCE = new TweetCollection();
+        }
+        return INSTANCE;
+    }
+
+    public void addTweet(String hashtag, Status tweet) {
+        long tweetId = tweet.getId();
+        if (!myTweets.getData().containsKey(tweetId)) {
+            MyTweet myTweet = MyTweet.convert(tweet);
+            myTweets.getData().put(tweetId, myTweet);
+
+            if(!myHashtags.getData().containsKey(hashtag)){
+                myHashtags.getData().put(hashtag, new ArrayList<Long>());
+            }
+            if(!myHashtags.getData().get(hashtag).contains(tweetId)) {
+                myHashtags.getData().get(hashtag).add(tweetId);
+            }
+
+            count++;
+            System.out.println(count + "\t" + tweet.getId());
+
+            save();
+        }
+    }
+
+    public void save() {
+        if(count == SAVE_COUNT) {
+            System.out.println("Save Data");
+            try {
+                myTweets.save();
+            } catch (FileNotFoundException e) {
+                System.err.println(e.getMessage());
+            }
+            try {
+                myHashtags.save();
+            } catch (FileNotFoundException e) {
+                System.err.println(e.getMessage());
+            }
+            count = 0;
+        }
+    }
+
+}
