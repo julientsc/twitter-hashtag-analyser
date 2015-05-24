603b390de665fc96d20dba8fb0283a613d650a02
Julien Tscherrig <julien.tscherrig@hefr.ch>
2015-05-24 02:49:45 +0200

    Update Config class with recordable

--- src/main/java/model/Config.java
+++ src/main/java/model/Config.java
@@ -2,49 +2,31 @@ package model;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import sun.org.mozilla.javascript.internal.ScriptRuntime;
 
 import java.io.*;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
  * Created by Julien on 23.05.15.
  */
-public class Config {
+public class Config extends Recordable<HashMap<String, ArrayList<String>>> {
 
-    public static final String path = "config.json";
-    private static Config instance = null;
+    public static final String PATH = "config.json";
+    public static final Class CLASS = new HashMap<String, ArrayList<String>>().getClass();
 
-    private HashMap<String, ArrayList<String>> configuration = null;
+    private static Config INSTANCE = null;
 
-    private static Config getInstance() {
-        if(instance == null) {
-            try {
-                instance = load(path);
-            }catch (Exception e) {
-                instance = new Config(path);
-            }
+    private Config(String path, Class c) throws IllegalAccessException, InvocationTargetException, InstantiationException {
+        super(path, c);
+    }
+
+    public static Config getInstance() throws Exception {
+        if(INSTANCE == null) {
+            INSTANCE = new Config(PATH, CLASS);
         }
-        return instance;
+        return INSTANCE;
     }
 
-    private static Config load(String path) throws IOException {
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        BufferedReader br = new BufferedReader(new FileReader(path));
-        instance = gson.fromJson(br, Config.class);
-        br.close();
-        return instance;
-    }
-
-    public Config() {
-        this.configuration = new HashMap<String, ArrayList<String>>();
-    }
-
-    public void save() {
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        PrintWriter pw = new PrintWriter(path);
-        pw.write(gson.toJson(instance));
-        pw.close();
-    }
 }
